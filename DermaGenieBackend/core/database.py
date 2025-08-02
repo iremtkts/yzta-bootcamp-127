af183e49e2838efa0cec17e7ea68a836b9a98d14
iremtektas <tektasirem13@gmail.com>
2025-08-02 23:25:16 +0300

    no message

--- DermaGenieBackend/core/database.py
+++ DermaGenieBackend/core/database.py
@@ -4,13 +4,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+if not DATABASE_URL:
+    raise ValueError("❌ DATABASE_URL environment variable is not set.")
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
